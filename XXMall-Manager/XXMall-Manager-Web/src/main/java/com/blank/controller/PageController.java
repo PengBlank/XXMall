c60e93d8877132c0a998cb3e07c656f496a2372b
Blank Peng <925137520@qq.com>
2018-06-25 21:49:48 +0800

    添加css和js文件，index.jsp

---
+++ XXMall-Manager/XXMall-Manager-Web/src/main/java/com/blank/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.blank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+  @RequestMapping("/")
+  public String indexPage() {
+    return "index";
+  }
+}
